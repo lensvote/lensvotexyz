e3e2762862159f0a4f7bfacd871f004f638cb6b1
Amagi:DDmxh <amagiddmxh@gmail.com>
2023-01-12 03:46:49 +0800

    Add transfer to property to detail card

--- apps/web/components/proposal/ProposalDetailCard.tsx
+++ apps/web/components/proposal/ProposalDetailCard.tsx
@@ -6,7 +6,7 @@ import {
 } from "@lib/hooks/useGovernorContract"
 import { useEffect, useState } from "react"
 import { formatDate } from "@lib/formats"
-import { formatEther } from "ethers/lib/utils.js"
+import { defaultAbiCoder, formatEther } from "ethers/lib/utils.js"
 import { BigNumber } from "ethers"
 
 type ProposalDetailCardProps = {
@@ -17,7 +17,19 @@ type ProposalDetailCardProps = {
 const ProposalDetailCard = ({ proposal, actions }: ProposalDetailCardProps) => {
   const { startBlock, endBlock } = proposal
   // There is only one action right now
-  const [transferTarget, transferAmount] = actions.map((acts) => acts[0])
+  const [transferTarget, transferAmount, _, calldata] = actions.map(
+    (acts) => acts[0],
+  )
+  // There is only two types of actions, either transferEther or transferErc20Token
+  // We can decode the value of transferFrom function's calldata from transferErc20Token action
+  const decodedData =
+    calldata !== "0x00"
+      ? (defaultAbiCoder.decode(["address", "uint256"], calldata as string) as [
+          string,
+          BigNumber,
+        ])
+      : null
+
   const provider = useProvider()
   // Formatted time string
   const [{ startTime, endTime }, setTimes] = useState<{
@@ -85,6 +97,15 @@ const ProposalDetailCard = ({ proposal, actions }: ProposalDetailCardProps) => {
               {formatEther(transferAmount.toString())}
             </p>
           </div>
+
+          {decodedData && (
+            <div className="flex flex-wrap justify-between text-sm capitalize">
+              <p className="text-[#868A8F]">Transfer to</p>
+              <p className="text-[#090909] flex-1 text-right break-all ml-8">
+                {decodedData[0]}
+              </p>
+            </div>
+          )}
         </div>
       </div>
     </Card>
